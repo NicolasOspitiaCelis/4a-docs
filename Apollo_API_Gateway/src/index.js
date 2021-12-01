024fc8918052785ecad2dea483afa3e6082a8a2c
transconductancia <carlos.rodridelghams@gmail.com>
2021-12-01 18:18:09 -0500

    agregados turnos

--- Apollo_API_Gateway/src/index.js
+++ Apollo_API_Gateway/src/index.js
@@ -1,6 +1,4 @@
-const {
-    ApolloServer
-} = require('apollo-server');
+const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
 const TurnosAPI = require('./dataSources/turnos_api');
